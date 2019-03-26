e2f0e4ad3e223777d73c3ab964420e4f5bc74248
SG <sgng1@e.ntu.edu.sg>
2019-03-26 02:32:34 +0000

    added actions and reducers

--- react/src/index.js
+++ react/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom";
 import TodoList from "./TodoList";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import reducer from "./reducers";
 
-const store = createStore();
+const store = createStore(reducer);
 
 function App() {
   return (
