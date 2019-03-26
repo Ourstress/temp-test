4e02077cafb27a677b18a31c028f08e8359deb89
SG <sgng1@e.ntu.edu.sg>
2019-03-26 09:04:12 +0000

    solved some DOM element bug hindering test from working

--- react/src/App.test.js
+++ react/src/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-testing-library";
 import "jest-dom/extend-expect";
-import App from "./index";
+import App from "./App";
 
 describe("The index component", () => {
   it("index component should render text initial state", () => {
